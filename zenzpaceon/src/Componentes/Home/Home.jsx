de10fab5dfbf8931ec706c91a68b61f95d4b7a85
Matheus Cruz <mccruz2611@gmail.com>
2021-09-30 20:35:48 -0300

    termino da base do home

--- zenzpaceon/src/Componentes/Home/Home.jsx
+++ zenzpaceon/src/Componentes/Home/Home.jsx
@@ -40,9 +40,37 @@ export default function Home() {
                     gestão inovadora da qual fazemos parte."
                 </p>
                 <cite>Lucas Vieira</cite>
-            </blockquote>
+                </blockquote>
+            </div>    
+            <div>
+                <p>Inicie sua experiência. Procure um profissional perfeito para você</p>
+                <a href="../pesquisa/pesquisa.html" class="btn">Pesquisa</a>
+            </div>
+            <div>
+                <h2>Suas próximas consultas</h2>
+            </div>
+            <div>
+                <img src="null" alt="Dr Sergio"/>
+                <p>24 de maio de 2021.</p>
+                <span>Dr. Sergio</span>
+            </div>
+             <div>
+                <img src="null" alt="Dr Sergio"/>
+                <p>28 de maio de 2021.</p>
+                <span>Dra. Cristina</span>
+            </div>
+            <div >
+                <img src="null" alt="Dr Sergio"/>
+                <p>06 de junho de 2021.</p>
+                <span>Dra. Julia</span>
+            </div>
+            <div>
+                <p>Acesse sua agenda para mais detalhes</p>
+                <a href="null">Agenda</a>
             </div>
         </div>
+    
+        
 
     )
 }
