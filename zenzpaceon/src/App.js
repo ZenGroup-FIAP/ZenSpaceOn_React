d104703d40c88194bcce8752fafdc949ca435123
Matheus Cruz <mccruz2611@gmail.com>
2021-09-29 23:16:45 -0300

    coloquei o menu e o rodape no app.js

--- zenzpaceon/src/App.js
+++ zenzpaceon/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Menu from './Componentes/Menu/menu'
+import Rodape from './Componentes/Rodape/Rodape'
 
 export default function App() {
   return (
     <div>
-      
+      <Menu/>
+      <Rodape/>
     </div>
   )
 }
