16d14ff8891b8bbac833e32af5b748945fe88409
Matheus Cruz <mccruz2611@gmail.com>
2021-10-01 10:06:35 -0300

    colocando os imports

--- zenzpaceon/src/Componentes/Home/Home.jsx
+++ zenzpaceon/src/Componentes/Home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Introducao from '.Introducao.jsx'
+import Autores from './Autores.jsx'
+import Agendamento from '../Agendamento/Agendamento.jsx'
+
 
 export default function Home() {
     return (
         <div>
-            
+            <Introducao />
+            <Autores />
+            <Agendamento />
         </div>
     )
 }
