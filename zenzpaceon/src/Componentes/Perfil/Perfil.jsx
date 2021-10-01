34af80d30203ef3f2d6aa7e37eb87d855158f278
Matheus Cruz <mccruz2611@gmail.com>
2021-10-01 08:42:10 -0300

    criacao da base do perfil

--- zenzpaceon/src/Componentes/Perfil/Perfil.jsx
+++ zenzpaceon/src/Componentes/Perfil/Perfil.jsx
@@ -3,7 +3,21 @@ import React from 'react'
 export default function Perfil() {
     return (
         <div>
-            
+            <div>
+                <img src="" alt="foto de perfil"/>
+                <div>
+                    <h1>Nome Exemplo</h1>
+                    <p>25 anos</p>
+                    <p>+55 11 9999-9999</p>
+                    <p>nome.exemplo@email.com</p>
+                </div>
+            </div>
+
+            <div>
+                <h3>Bio</h3>
+                <p>Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore culpa voluptates quasi ea eligendi inventore ex, ratione quis quas cumque mollitia, totam ipsa consequatur asperiores, assumenda error excepturi doloribus voluptate! consectetur adipisicing elit. Eaque molestiae eius et labore explicabo necessitatibus corrupti asperiores ipsum nobis ullam deleniti vitae aliquid nisi, obcaecati ratione? Debitis, maiores! Illum, quis?</p>
+                <a href="#">editar perfil</a>
+            </div> 
         </div>
     )
 }
