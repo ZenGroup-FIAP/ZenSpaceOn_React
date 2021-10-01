172fd8c783028664fd27570d8501ca9ac30e8180
Matheus Cruz <mccruz2611@gmail.com>
2021-10-01 08:10:20 -0300

    fazendo a parte dos autores

---
+++ zenzpaceon/src/Componentes/Home/Autores.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function Autores() {
+    return (
+        <div>
+            <div>
+                <blockquote>
+                    <p>"No mundo atual, a contínua expansão de nossa
+                        atividade cumpre um papel essencial na formulação da
+                        gestão inovadora da qual fazemos parte.
+                        No mundo atual, a contínua expansão de nossa
+                        atividade cumpre um papel essencial na formulação da
+                        gestão inovadora da qual fazemos parte."
+                    </p>
+                    <cite>Alexandre Dinis</cite>
+                </blockquote>
+        
+                <blockquote>
+                    <p>"No mundo atual, a contínua expansão de nossa
+                        atividade cumpre um papel essencial na formulação da
+                        gestão inovadora da qual fazemos parte.
+                        No mundo atual, a contínua expansão de nossa
+                        atividade cumpre um papel essencial na formulação da
+                        gestão inovadora da qual fazemos parte."
+                    </p>
+                    <cite>Barbara Nogueira</cite>
+                </blockquote>
+
+                <blockquote>
+                <p>"No mundo atual, a contínua expansão de nossa
+                    atividade cumpre um papel essencial na formulação da
+                    gestão inovadora da qual fazemos parte.
+                    No mundo atual, a contínua expansão de nossa
+                    atividade cumpre um papel essencial na formulação da
+                    gestão inovadora da qual fazemos parte."
+                </p>
+                <cite>Lucas Vieira</cite>
+                </blockquote>
+            </div> 
+        </div>
+    )
+}
--- zenzpaceon/src/Componentes/Home/Home.jsx
+++ zenzpaceon/src/Componentes/Home/Home.jsx
@@ -8,40 +8,7 @@ export default function Home() {
                 <h3>ZenSpace ON</h3>
                 <p>O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham passagens com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker que incluem versões do Lorem Ipsum.</p>
             </div>
-            <div>
-                <blockquote>
-                    <p>"No mundo atual, a contínua expansão de nossa
-                        atividade cumpre um papel essencial na formulação da
-                        gestão inovadora da qual fazemos parte.
-                        No mundo atual, a contínua expansão de nossa
-                        atividade cumpre um papel essencial na formulação da
-                        gestão inovadora da qual fazemos parte."
-                    </p>
-                    <cite>Alexandre Dinis</cite>
-                </blockquote>
-        
-                <blockquote>
-                    <p>"No mundo atual, a contínua expansão de nossa
-                        atividade cumpre um papel essencial na formulação da
-                        gestão inovadora da qual fazemos parte.
-                        No mundo atual, a contínua expansão de nossa
-                        atividade cumpre um papel essencial na formulação da
-                        gestão inovadora da qual fazemos parte."
-                    </p>
-                    <cite>Barbara Nogueira</cite>
-                </blockquote>
-
-                <blockquote>
-                <p>"No mundo atual, a contínua expansão de nossa
-                    atividade cumpre um papel essencial na formulação da
-                    gestão inovadora da qual fazemos parte.
-                    No mundo atual, a contínua expansão de nossa
-                    atividade cumpre um papel essencial na formulação da
-                    gestão inovadora da qual fazemos parte."
-                </p>
-                <cite>Lucas Vieira</cite>
-                </blockquote>
-            </div>    
+               
             <div>
                 <p>Inicie sua experiência. Procure um profissional perfeito para você</p>
                 <a href="../pesquisa/pesquisa.html" class="btn">Pesquisa</a>
