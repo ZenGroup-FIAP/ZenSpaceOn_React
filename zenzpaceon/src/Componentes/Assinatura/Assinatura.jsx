11a4287a099bd96446179a759420fdb4eb99460b
Matheus Cruz <mccruz2611@gmail.com>
2021-10-01 09:07:17 -0300

    termino da base asinatura.jsx

--- zenzpaceon/src/Componentes/Assinatura/Assinatura.jsx
+++ zenzpaceon/src/Componentes/Assinatura/Assinatura.jsx
@@ -3,6 +3,34 @@ import React from 'react'
 export default function Assinatura() {
     return (
         <div>
+            <div>
+                <div>
+                    <h2>R$ 100</h2>
+                </div>
+                <h3>BÁSICO</h3>
+                <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            </div>
+
+            <div>
+                <div>
+                    <h2>R$ 150</h2>
+                </div>
+                <div>
+                    <h3>INTERMEDIÁRIO</h3>
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <h2>R$ 200</h2>
+                </div>
+                <div>
+                    <h3>COMPLETO</h3>
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+                </div>  
+            
+            </div>
             
         </div>
     )
