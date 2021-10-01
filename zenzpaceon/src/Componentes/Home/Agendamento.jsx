c4c00ca5c1935145ca20850a543f4283d38d6e27
Matheus Cruz <mccruz2611@gmail.com>
2021-10-01 08:17:21 -0300

    dividindo a home em tres

--- zenzpaceon/src/Componentes/Home/Agendamento.jsx
+++ zenzpaceon/src/Componentes/Home/Agendamento.jsx
@@ -1,14 +1,8 @@
 import React from 'react'
 
-export default function Home() {
+export default function Agendamento() {
     return (
         <div>
-            <div>
-                <h1>Consultas Online de forma fácil</h1>
-                <h3>ZenSpace ON</h3>
-                <p>O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham passagens com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker que incluem versões do Lorem Ipsum.</p>
-            </div>
-               
             <div>
                 <p>Inicie sua experiência. Procure um profissional perfeito para você</p>
                 <a href="../pesquisa/pesquisa.html" class="btn">Pesquisa</a>
@@ -21,12 +15,12 @@ export default function Home() {
                 <p>24 de maio de 2021.</p>
                 <span>Dr. Sergio</span>
             </div>
-             <div>
+            <div>
                 <img src="null" alt="Dr Sergio"/>
                 <p>28 de maio de 2021.</p>
                 <span>Dra. Cristina</span>
             </div>
-            <div >
+            <div>
                 <img src="null" alt="Dr Sergio"/>
                 <p>06 de junho de 2021.</p>
                 <span>Dra. Julia</span>
@@ -36,8 +30,5 @@ export default function Home() {
                 <a href="null">Agenda</a>
             </div>
         </div>
-    
-        
-
     )
 }
