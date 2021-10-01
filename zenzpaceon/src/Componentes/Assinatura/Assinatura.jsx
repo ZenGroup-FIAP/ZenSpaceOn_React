9433d6b856fa960a6d21ca86c1a3bb8750837f55
Matheus Cruz <mccruz2611@gmail.com>
2021-10-01 10:06:08 -0300

    importando dados

--- zenzpaceon/src/Componentes/Assinatura/Assinatura.jsx
+++ zenzpaceon/src/Componentes/Assinatura/Assinatura.jsx
@@ -1,36 +1,11 @@
 import React from 'react'
+import AssinaturaPlano from './AssinaturaPlano.jsx'
+
 
 export default function Assinatura() {
     return (
         <div>
-            <div>
-                <div>
-                    <h2>R$ 100</h2>
-                </div>
-                <h3>BÁSICO</h3>
-                <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
-            </div>
-
-            <div>
-                <div>
-                    <h2>R$ 150</h2>
-                </div>
-                <div>
-                    <h3>INTERMEDIÁRIO</h3>
-                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
-                </div>
-            </div>
-
-            <div>
-                <div>
-                    <h2>R$ 200</h2>
-                </div>
-                <div>
-                    <h3>COMPLETO</h3>
-                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
-                </div>  
-            
-            </div>
+            <AssinaturaPlano />
             
         </div>
     )
