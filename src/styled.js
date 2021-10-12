082d3f37f7fe8292667d6acddd73644f029020bf
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-10-12 00:08:04 -0300

    Estilização do menu

--- src/styled.js
+++ src/styled.js
@@ -1,1 +1,24 @@
-import styled from 'styled-component';
+import styled from 'styled-components';
+
+export const HeaderStyled = styled.header`
+    background-color: #17d19c;
+    color: #fff;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px;
+
+    a {
+        color: #fff;
+        text-decoration: none;
+    }
+
+    ul {
+        list-style: none;
+        display: flex;
+    }
+
+    ul li {
+        margin: 0 10px;
+    }
+`
