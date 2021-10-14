7cb1b0939acec58e9930ec34a93b8bb5b0670a11
matsu1730 <rm87736@fiap.com.br>
2021-10-13 22:03:26 -0300

    Alterando Rodape e revisão de código

--- src/Componentes/Sobre/Sobre.jsx
+++ src/Componentes/Sobre/Sobre.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SaibaMais from './SaibaMais.jsx'
 import Valores from './Valores.jsx'
+import Quote from '../Quote/Quote.jsx'
 
 export default function Sobre() {
     return (
         <div>
             <SaibaMais/>
             <Valores/>
+            <Quote />
         </div>
     )
 }
