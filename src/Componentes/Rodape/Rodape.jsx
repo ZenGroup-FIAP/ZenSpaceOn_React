a6c274160b5c91dd6e9644903c4cefe563759d3d
matsu1730 <rm87736@fiap.com.br>
2021-10-18 19:14:11 -0300

    Finalização Home

--- src/Componentes/Rodape/Rodape.jsx
+++ src/Componentes/Rodape/Rodape.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { FooterText, BgFooter, Container } from '../../styled.js'
 
 export default function Rodape() {
     return (
         <div>
-            <footer>
-                <div>
-                    <small>ZenSpace ON 2021 - Alguns direitos reservados.</small>
-                </div>
-            </footer>
+            <BgFooter>
+                <Container>
+                    <div>
+                        <FooterText>ZenSpace ON 2021 - Alguns direitos reservados.</FooterText>
+                    </div>
+                </Container>
+            </BgFooter>
         </div>
     )
 }
